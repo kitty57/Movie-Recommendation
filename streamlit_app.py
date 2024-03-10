f0ad07cc72215987a91390f99d9e52ffd6896e17
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-11 00:26:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
+from google.generativeai import GenerativeModel
+from IPython.display import Markdown
+import textwrap
 
-st.title('🎈 App Name')
+genai.configure(api_key='AIzaSyDlBFVsmV8pao6Ax-bcR0dc5h4CusiNCsc')
 
-st.write('Hello world!')
+def prompt(movie_name):
+    prompt_parts = [
+        f"'As a seasoned movie critic with a broad spectrum of cinematic exposure across languages,"
+        f"recommend five movies similar to the given movie-Movie name: {movie_name},"
+        f"Also for each movie explain why you find it similar to the given movie'",
+    ]
+    return prompt_parts
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+  
+def recommend(movie_name, model):
+    human_prompt = prompt(movie_name)
+    response = model.generate_content(human_prompt)
+    return response.text
+def main():
+    st.title('Movie Recommendation App')
+    movie_name = st.text_input('Enter the movie name:')
+    if movie_name:
+        model = GenerativeModel(model_name="gemini-pro")
+        response = recommend(movie_name, model)
+        st.markdown(to_markdown(response))
+if __name__ == '__main__':
+    main()
+
